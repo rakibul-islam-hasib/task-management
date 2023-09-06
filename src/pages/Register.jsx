f7ffa6f66449420020fbcf284dff39f7e4248e3e
Hasib Hossain <boraborhasib@gmail.com>
2023-09-06 22:47:52 +0600

    Updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ const Register = () => {
     return (
         <div>
             <div className="flex items-center justify-center h-screen">
-                <div className="flex flex-col">
+                <form className="flex flex-col">
                     <div className="relative">
                         <input type="text" id="username" className="border-b w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
                         <label htmlFor="username" className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Username</label>
@@ -25,7 +25,10 @@ const Register = () => {
                             htmlFor="photoURL"
                             className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Photo URL</label>
                     </div>
-                </div>
+                    <div className="mt-5">
+                        <button className='w-[300px] bg-purple-600 text-white font-bold py-3'>Register</button>
+                    </div>
+                </form>
             </div>
 
         </div>
