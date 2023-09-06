a36356b46a28b519c97a17ad9718a941f544c98e
Hasib Hossain <boraborhasib@gmail.com>
2023-09-06 22:13:14 +0600

    Firebase config added

---
+++ src/config/firebase.config.cjs
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArhM0exiWMKtsOZIzbiFJzH_IwmjKtkEY",
+  authDomain: "task-management-cb181.firebaseapp.com",
+  projectId: "task-management-cb181",
+  storageBucket: "task-management-cb181.appspot.com",
+  messagingSenderId: "385310284060",
+  appId: "1:385310284060:web:e3930fa28fa3c3577602d9"
+};
+
+// Initialize Firebase
+export  const app = initializeApp(firebaseConfig);
---
+++ src/contex/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({children}) => {
+    
+    const contextValue = {};
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
