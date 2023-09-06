91f33c0e55bd8262c4de0678e24da1d0e351ccc1
Hasib Hossain <boraborhasib@gmail.com>
2023-09-06 21:45:12 +0600

    Register page added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div>
-            <h1 className='px-1'>Main Layout </h1>
+            <Outlet />
         </div>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <Link to='/register'>Register</Link>
         </div>
     );
 };
