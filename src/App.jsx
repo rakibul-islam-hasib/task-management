c934cfaa9e97f080907d5372f971db8fee938c3b
Hasib Hossain <boraborhasib@gmail.com>
2023-09-06 21:35:28 +0600

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>React App</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="relative">
+          <input type="text" id="username" className="border-b py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
+            <label htmlFor="username" className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Username</label>
+        </div>
+      </div>
+      <h1></h1>
     </div>
   );
 };
