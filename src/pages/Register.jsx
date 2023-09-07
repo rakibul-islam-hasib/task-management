961a237c175006200b11a2e9fa4b191a92c1f696
Hasib Hossain <boraborhasib@gmail.com>
2023-09-07 10:11:12 +0600

    Updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
+import { useAuth } from '../hooks/useAuth';
 
 const Register = () => {
+    const { registerUser } = useAuth();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const formData = Object.fromEntries(data.entries());
+        console.log(formData);
+        registerUser(formData.email, formData.password);
+    }
+
     return (
         <div>
             <div className="flex items-center justify-center h-screen">
-                <form className="flex flex-col">
+                <form onSubmit={e => handleSubmit(e)} className="flex flex-col">
                     <div className="relative">
-                        <input type="text" id="username" className="border-b w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
-                        <label htmlFor="username" className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Username</label>
+                        <input
+                            placeholder='username'
+                            type="email"
+                            required
+                            name='email'
+                            className="border-b mt-5 placeholder:text-purple-600 w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
+
                     </div>
-                    <div className="relative mt-5">
                         <input
                             type="text"
                             id="bio"
-                            className="border-b w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
-                        <label htmlFor="bio" className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Bio</label>
-                    </div>
-                    <div className="relative mt-5">
+                            name='bio'
+                            placeholder='bio'
+                            className="border-b mt-5 placeholder:text-purple-600 w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
                         <input
                             type="text"
                             id="photoURL"
-                            className="border-b w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
-                        <label
-                            htmlFor="photoURL"
-                            className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Photo URL</label>
-                    </div>
+                            name='photoURL'
+                            placeholder='photoURL'
+                            className="border-b mt-5 placeholder:text-purple-600 w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
+
+                    <input
+                        type="password"
+                        name='password'
+                        placeholder='password'
+                        className="border-b mt-5 placeholder:text-purple-600 w-[300px] py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
+
                     <div className="mt-5">
-                        <button className='w-[300px] bg-purple-600 text-white font-bold py-3'>Register</button>
+                        <button type='submit' className='w-[300px] bg-purple-600 text-white font-bold py-3'>Register</button>
                     </div>
                 </form>
             </div>
